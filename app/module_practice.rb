d027cbce53ce628e74b4d8ac71b53100e555a3b4
Masashi Fujita <masashifujita536@gmail.com>
2023-10-12 03:37:53 +0900

    add module spec

--- app/module_practice.rb
+++ app/module_practice.rb
@@ -1,1 +1,22 @@
-class ModulePractice; end
+module ModulePractice
+  class ParentClass
+    def i_method
+      "親クラスのインスタンスメソッド"
+    end
+
+    def self.c_method
+      "親クラスのクラスメソッド"
+    end
+  end
+
+  class ChildClass < ParentClass
+    def i_method
+      "子クラスのインスタンスメソッド"
+    end
+
+    def self.c_method
+      "子クラスのクラスメソッド"
+    end
+  end
+end
+
--- spec/module_practice_spec.rb
+++ spec/module_practice_spec.rb
@@ -1,8 +1,49 @@
 require 'spec_helper'
 
 RSpec.describe ModulePractice do
-  it 'てすと' do
-    binding.b
-    expect(true).to be_truthy
+  let!(:parent_class){ ModulePractice::ParentClass }
+  let!(:child_class){ ModulePractice::ParentClass }
+
+  describe 'object' do
+    let!(:obj){ child_class.new }
+
+    describe '特異メソッド' do
+      # obj.singleton_class
+      it '「特異クラス < 元のクラス」であること' do
+        expect(obj.singleton_class.superclass).to eq obj.class
+      end
+
+      it '他のオブジェクトは特異メソッドを呼び出せないこと' do
+        obj.define_singleton_method(:added_method){ "特異メソッド" }
+        another_obj = child_class.new
+        expect(another_obj.respond_to?(:added_method)).to be_falsy
+      end
+
+      # obj.define_singleton_method(:method){ ・・・ }
+      it 'クラスに定義したインスタンスメソッドより特異メソッドが優先的に呼ばれること' do
+        obj.define_singleton_method(:i_method){ "特異メソッド" }
+        expect(obj.i_method).to eq "特異メソッド"
+      end
+
+      # module.instance_methods(false)
+      it 'オブジェクトの特異メソッドが特異クラスに定義されたインスタンスメソッドであること' do
+        obj.define_singleton_method(:i_method){ "特異メソッド" }
+        s_methods = obj.singleton_class.instance_methods(false)
+        expect(s_methods).to eq [:i_method]
+      end
+    end
+
+    describe 'extend' do
+      it 'クラスメソッド' do; end
+    end
   end
+
+  describe 'class' do
+    describe 'include' do; end
+    describe 'prepend' do; end
+    describe 'monkey_patch' do; end
+    describe 'refinements' do; end
+  end
+
+  describe 'module' do; end
 end
